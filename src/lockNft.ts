5d53f4c739ef118b86d726033975812704015c2f
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-02-21 02:39:27 +0700

    dynamically bind selected nft-id for unlock

--- src/lockNft.ts
+++ src/lockNft.ts
@@ -2,23 +2,28 @@ import { contract, web3 } from "./utils/contract";
 import { injectHandler } from "./utils/helper";
 import config from './utils/config';
 
-const lockNft = async () => {
-	const blockTimestamp = await web3.eth.getBlock("latest");
-	const expired = blockTimestamp.timestamp + BigInt(600);
+const lockNft = async (nftId: number) => {
+  const blockTimestamp = await web3.eth.getBlock("latest");
+  const expired = blockTimestamp.timestamp + BigInt(600);
 
-	const encodeABI = await contract.methods.lock(config.nftManagerAddress, 4, 1, expired).encodeABI();
-	const tx = {
-		from: config.nftManagerAddress,
-		to: contract._address,
-		gasPrice: web3.utils.toWei('20', 'gwei'),
-		gasLimit: 200000,
-		data: encodeABI,
-	};
+  const encodeABI = await contract.methods.lock(config.nftManagerAddress, nftId, 1, expired).encodeABI();
+  const tx = {
+    from: config.nftManagerAddress,
+    to: contract._address,
+    gasPrice: web3.utils.toWei('20', 'gwei'),
+    gasLimit: 200000,
+    data: encodeABI,
+  };
 
-	const signedTx = await web3.eth.accounts.signTransaction(tx, config.nftManagerKey);
-	web3.eth.sendSignedTransaction(signedTx.rawTransaction).on('receipt', () => {
-		location.href = './game/index.html';
-	});
+  const signedTx = await web3.eth.accounts.signTransaction(tx, config.nftManagerKey);
+  web3.eth.sendSignedTransaction(signedTx.rawTransaction).on('receipt', () => {
+    localStorage.setItem('selectedNftId', String(nftId));
+    location.href = './game/index.html';
+  });
 }
 
-injectHandler('char4', lockNft);
+injectHandler('char1', () => lockNft(1));
+injectHandler('char2', () => lockNft(2));
+injectHandler('char3', () => lockNft(3));
+injectHandler('char4', () => lockNft(4));
+injectHandler('char5', () => lockNft(5));
--- src/unlockNft.ts
+++ src/unlockNft.ts
@@ -2,18 +2,19 @@ import { contract, web3 } from "./utils/contract";
 import config from './utils/config';
 
 const unlockNft = async () => {
-	const challengerAddress = localStorage.getItem('CHALLENGER-ACCOUNT');
-	const encodeABI = await contract.methods.unlockAndTransfer(config.nftManagerAddress, challengerAddress, 4, 1, 1, []).encodeABI();
-	const tx = {
-		from: config.nftManagerAddress,
-		to: contract._address,
-		gasPrice: web3.utils.toWei('20', 'gwei'),
-		gasLimit: 200000,
-		data: encodeABI,
-	};
+  const challengerAddress = localStorage.getItem('CHALLENGER-ACCOUNT');
+  const nftId = parseInt(localStorage.getItem('selectedNftId') as string);
+  const encodeABI = await contract.methods.unlockAndTransfer(config.nftManagerAddress, challengerAddress, nftId, 1, 1, []).encodeABI();
+  const transaction = {
+    from: config.nftManagerAddress,
+    to: contract._address,
+    gasPrice: web3.utils.toWei('20', 'gwei'),
+    gasLimit: 200000,
+    data: encodeABI,
+  };
 
-	const signedTx = await web3.eth.accounts.signTransaction(tx, config.nftManagerKey);
-	web3.eth.sendSignedTransaction(signedTx.rawTransaction).on('receipt', console.log);
+  const signedTx = await web3.eth.accounts.signTransaction(transaction, config.nftManagerKey);
+  web3.eth.sendSignedTransaction(signedTx.rawTransaction).on('receipt', console.log);
 }
 
 unlockNft();
