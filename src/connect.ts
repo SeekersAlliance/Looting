b29978d1ef6e5ac50320a6a36eb70c2c5a0908c7
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-02-22 03:47:22 +0700

    smart redirect connected player, prevent asking for re-connect once connected

--- src/connect.ts
+++ src/connect.ts
@@ -1,49 +1,49 @@
 import { injectHandler } from './utils/helper';
 
-const connectWallet = async () => {
-  const chainConfig = {
-    chainId: '0x15EB',
-    chainName: 'opBNB Testnet',
-    nativeCurrency: {
-      symbol: 'tBNB',
-      decimals: 18,
-    },
-    rpcUrls: ['https://opbnb-testnet-rpc.bnbchain.org'],
-    blockExplorerUrls: ['https://opbnb-testnet.bscscan.com'],
-  }
+const targetChainConfig = {
+  chainId: '0x15EB',
+  chainName: 'opBNB Testnet',
+  nativeCurrency: {
+    symbol: 'tBNB',
+    decimals: 18,
+  },
+  rpcUrls: ['https://opbnb-testnet-rpc.bnbchain.org'],
+  blockExplorerUrls: ['https://opbnb-testnet.bscscan.com'],
+}
 
+const redirectConnectedPlayer = async () => {
+  const [account] = await window.ethereum.request({ method: 'eth_accounts' })
+
+  if (account) {
+    localStorage.setItem('CHALLENGER-ACCOUNT', account);
+    window.location.href = './view-wallet.html';
+  }
+};
+
+const connectWallet = async () => {
   try {
-    console.log('start');
-    const accounts = await window.ethereum
-      ?.request({
-        method: 'wallet_requestPermissions',
-        params: [
-          {
-            eth_accounts: {},
-          }
-        ]
-      });
-    const account = accounts?.[0]?.caveats?.[0].value[0];
+    const [account] = await window.ethereum?.request({ method: 'eth_requestAccounts' });
     localStorage.setItem('CHALLENGER-ACCOUNT', account);
 
     await window.ethereum?.request({
-          method: 'wallet_addEthereumChain',
-          params: [chainConfig],
-        });
+      method: 'wallet_addEthereumChain',
+      params: [targetChainConfig],
+    });
 
     await window.ethereum?.request({
-          method: 'wallet_switchEthereumChain',
-          params: [
-            {
-              chainId: chainConfig.chainId,
-            },
-          ],
-        }).then(() => {
-          window.location.href = './view-wallet.html';
-        });
+      method: 'wallet_switchEthereumChain',
+      params: [
+        {
+          chainId: targetChainConfig.chainId,
+        },
+      ],
+    }).then(() => {
+      window.location.href = './view-wallet.html';
+    });
   } catch (err) {
     console.log('Unable to retrieve account', err);
   }
 };
 
-injectHandler('index-connect-button', connectWallet)
+injectHandler('index-connect-button', connectWallet);
+redirectConnectedPlayer();
