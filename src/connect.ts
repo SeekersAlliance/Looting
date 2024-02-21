763b77000e511c879fff32dda8b82d1583974d28
FME849 <1414439@gmail.com>
2024-02-21 10:01:58 +0700

    feat: impl add and switch chain when connect wallet

--- src/connect.ts
+++ src/connect.ts
@@ -1,15 +1,49 @@
 import { injectHandler } from './utils/helper';
 
 const connectWallet = async () => {
-	const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const chainConfig = {
+    chainId: '0x15EB',
+    chainName: 'opBNB Testnet',
+    nativeCurrency: {
+      symbol: 'tBNB',
+      decimals: 18,
+    },
+    rpcUrls: ['https://opbnb-testnet-rpc.bnbchain.org'],
+    blockExplorerUrls: ['https://opbnb-testnet.bscscan.com'],
+  }
 
-	if (!account) {
-		console.log('unable to get account');
-		return;
-	}
+  try {
+    console.log('start');
+    const accounts = await window.ethereum
+      ?.request({
+        method: 'wallet_requestPermissions',
+        params: [
+          {
+            eth_accounts: {},
+          }
+        ]
+      });
+    const account = accounts?.[0]?.caveats?.[0].value[0];
+    localStorage.setItem('CHALLENGER-ACCOUNT', account);
 
-	localStorage.setItem('CHALLENGER-ACCOUNT', account);
-	location.href = './view-wallet.html';
+    await window.ethereum?.request({
+          method: 'wallet_addEthereumChain',
+          params: [chainConfig],
+        });
+
+    await window.ethereum?.request({
+          method: 'wallet_switchEthereumChain',
+          params: [
+            {
+              chainId: chainConfig.chainId,
+            },
+          ],
+        }).then(() => {
+          window.location.href = './view-wallet.html';
+        });
+  } catch (err) {
+    console.log('Unable to retrieve account', err);
+  }
 };
 
 injectHandler('index-connect-button', connectWallet)
