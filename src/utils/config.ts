78569e5f6672cc7ff1ecaa391fc4cf0f8d017894
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-02-23 02:02:12 +0700

    chore: use new contract address

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export default {
-	contractAddress: '0x0288Ca4FE6E5645464aA1C95F4E4EaAAc0F005C8',
-	nftManagerKey: 'dd86d28357f97aa127ecfea9b675c247ea02f877da7b26d99821e332edb4c5d4',
-	nftManagerAddress: '0x21ba0aE36317C9382fe374F819995F45a99ceEC5',
-	challengedAddress: '0x0288Ca4FE6E5645464aA1C95F4E4EaAAc0F005C8',
+  contractAddress: '0x54DEB0B88fcc483CFd06Ba8729D98a269454eDE9',
+  nftManagerKey: 'dd86d28357f97aa127ecfea9b675c247ea02f877da7b26d99821e332edb4c5d4',
+  nftManagerAddress: '0x21ba0aE36317C9382fe374F819995F45a99ceEC5',
+  challengedAddress: '0x21ba0aE36317C9382fe374F819995F45a99ceEC5',
 };
