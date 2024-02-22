8201887dfb98125ac828f8ed80e3fced6a75447f
FME849 <1414439@gmail.com>
2024-02-22 16:27:16 +0700

    feat: make add and switch chain work again

--- src/connect.ts
+++ src/connect.ts
@@ -12,12 +12,26 @@ const targetChainConfig = {
 }
 
 const redirectConnectedPlayer = async () => {
-  const [account] = await window.ethereum.request({ method: 'eth_accounts' })
+  const [account] = await window.ethereum.request({ method: 'eth_accounts' });
 
-  if (account) {
-    localStorage.setItem('CHALLENGER-ACCOUNT', account);
-    window.location.href = './view-wallet.html';
-  }
+  await window.ethereum?.request({
+    method: 'wallet_addEthereumChain',
+    params: [targetChainConfig],
+  });
+
+  await window.ethereum?.request({
+    method: 'wallet_switchEthereumChain',
+    params: [
+      {
+        chainId: targetChainConfig.chainId,
+      }
+    ]
+  }).then(() => {
+    if (account) {
+      localStorage.setItem('CHALLENGER-ACCOUNT', account);
+      window.location.href = './view-wallet.html';
+    }
+  });
 };
 
 const connectWallet = async () => {
@@ -38,7 +52,9 @@ const connectWallet = async () => {
         },
       ],
     }).then(() => {
-      window.location.href = './view-wallet.html';
+      if (account) {
+        window.location.href = './view-wallet.html';
+      }
     });
   } catch (err) {
     console.log('Unable to retrieve account', err);
